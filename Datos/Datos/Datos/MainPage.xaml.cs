33777bc9810e8c70c06c51931be376c270c0b358
Hector Lopez <uffw@outlook.com>
2017-09-11 22:57:20 -0500

    Practica 3 Dispositivos Moviles

---
+++ Datos/Datos/Datos/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Datos
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnpagina2.Clicked += Btnpagina2_Clicked;
+        }
+
+        private void Btnpagina2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Page2(txtusername.Text,txtape.Text));
+            
+        }
+    }
+}
---
+++ Datos/Datos/Datos/Page2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Datos
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string username ,string apellidos)
+        {
+            InitializeComponent();
+            txtusername.Text = username;
+            txtape.Text = apellidos;
+            btnpagina1.Clicked += Btnpagina1_Clicked;
+        }
+
+        private void Btnpagina1_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
