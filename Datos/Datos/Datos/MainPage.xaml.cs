0060020da8c4720621a881bb4b4591860a826e56
Hector Lopez <uffw@outlook.com>
2017-09-13 11:11:03 -0500

    Avance Practica 3

--- Datos/Datos/Datos/MainPage.xaml.cs
+++ Datos/Datos/Datos/MainPage.xaml.cs
@@ -14,12 +14,32 @@ namespace Datos
         {
             InitializeComponent();
             btnpagina2.Clicked += Btnpagina2_Clicked;
+            Prueba.Items.Add("Ingeniería en Sistemas Computacionales");
+            Prueba.Items.Add("Licenciatura en Administración");
+            Prueba.Items.Add("Ingeniería Industrial");
+            Prueba.Items.Add("Ingeniería Civil");
+            Prueba.Items.Add("Licenciatura en Gastronomia");
+            Prueba.Items.Add("Ingeniería Mecatrónica");
+            Prueba.Items.Add("Licenciatura en Biología");
+
+
+
+
+
+
+            
         }
 
         private void Btnpagina2_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushModalAsync(new Page2(txtusername.Text,txtape.Text));
+            var nombre = Prueba.Items[Prueba.SelectedIndex];
+            Navigation.PushModalAsync(new Page2(txtusername.Text,txtape.Text,txtdirec.Text,txtphone.Text,nombre,txtsemestre.Text,txtmatricula.Text,txtusuario.Text,txtcorreo.Text,txtgithub.Text));
+
+
             
         }
+
+   
+        
     }
 }
--- Datos/Datos/Datos/Page2.xaml.cs
+++ Datos/Datos/Datos/Page2.xaml.cs
@@ -12,11 +12,19 @@ namespace Datos
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Page2 : ContentPage
     {
-        public Page2(string username ,string apellidos)
+        public Page2(string nombre ,string apellidos,string direccion,string telefono,string carrera,string semestre,string matricula,string usuario,string correo,string github)
         {
             InitializeComponent();
-            txtusername.Text = username;
+            txtusername.Text = nombre;
             txtape.Text = apellidos;
+            txtdirec.Text = direccion;
+            txtphone.Text = telefono;
+            txtcarrera.Text = carrera;
+            txtsemestre.Text = semestre;
+            txtmatricula.Text = matricula;
+            txtusuario.Text = usuario;
+            txtcorreo.Text = correo;
+            txtgithub.Text = github;
             btnpagina1.Clicked += Btnpagina1_Clicked;
         }
 
